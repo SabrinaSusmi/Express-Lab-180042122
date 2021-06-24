58309a89f3413b05be0b5c95410b69626623fab7
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-24 11:58:14 +0600

    app.js connected to index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,5 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
 const PORT = 2122;
-
-app.get("/", (req, res) => {
-  res.send("<H1>Home Page</H1>");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}.`);
