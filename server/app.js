e3e4d66e21b28452d93e561627bb674e27af1925
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-24 12:12:48 +0600

    status code added, used post method

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<H1>Home Page</H1>");
+    res.status(200).send("<H1>Home Page - GET Request</H1>");
+});
+
+app.post("/", (req, res) => {
+    res.status(200).send("<H1>Home Page - POST Request</H1>");
 });
 
 app.get("/register", (req, res) => {
@@ -18,7 +22,7 @@ app.get("/dashboard", (req, res) => {
 });
 
 app.use((req, res) => {
-    res.send("Page doesn't exist!");
+    res.status(404).send("Page doesn't exist!");
   });
 
 module.exports = app;
