69483cf82b6bb8e9047388a6531dd815e6b9821f
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-25 01:13:30 +0600

    css added, home modified

--- server/app.js
+++ server/app.js
@@ -2,29 +2,11 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./routes/userRoutes.routes");
 
-app.use(userRoutes);
+app.use("/users/",userRoutes);
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.status(200).send("<H1>Home Page - GET Request</H1>");
-});
-
-app.post("/", (req, res) => {
-    res.status(200).send("<H1>Home Page - POST Request</H1>");
-});
-
-app.get("/register", (req, res) => {
-    //res.cookie("username", "susmi");
-    //res.clearCookie("username");
-    res.append("username", "susmi"); // info passing in the header
-    res.send("<H1>Register</H1>");
-});
-
-app.get("/login", (req, res) => {
-    res.json({ name: "Susmi", profession: "Student" });
-});
-
-app.get("/dashboard", (req, res) => {
-    res.send("<H1>Dashboard</H1>");
+    res.status(200).sendFile("home.html", { root: "./views" });
 });
 
 app.use((req, res) => {
