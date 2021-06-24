02f4af7b3c19f6c9eea550b3e699316f499b6b6a
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-24 11:56:33 +0600

    app.js created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 2122;
 
+app.get("/", (req, res) => {
+  res.send("<H1>Home Page</H1>");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}.`);
 });
