3397b964caeb255139713fee633251e0e1178a78
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-24 15:37:51 +0600

    Connected to mongoDb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
+const mongoose = require('mongoose')
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}.`);
 });
+
+mongoose.connect(process.env.DATABASE_ACCESS, () =>console.log('DB connected'))
+.catch((error) => console.log(error.message));
