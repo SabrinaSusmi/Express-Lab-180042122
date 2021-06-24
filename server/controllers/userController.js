1c84667a0d775b29263a012d660d7da31b2dd224
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-24 20:27:56 +0600

    retype pass validation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const registerForm = require('../models/userModel.models');
+const alert = require('alert');
 
 const getRegister = (req,res)=> {
     res.sendFile("register.html", { root: "./views/users" });
@@ -6,14 +7,23 @@ const getRegister = (req,res)=> {
 
 const postRegister = (req,res)=>{
 
-    const {username, email, gender, password} = req.body
+    const {username, email, gender, password, repassword } = req.body
 
-    if(!username || !email || !gender || !password) {
-        return res.json({msg: "Fill in the empty fields!"});
+    if(!username || !email || !gender || !password || !repassword) {
+        //alert("Fill in the empty fields!");
+        //window.alert("Fill in the empty fields!");
+        res.json({msg: "Fill in the empty fields!"});
+        return res.redirect("/register");
     }
     if(password.length < 6){
         return res.json({msg: "Password must be atleast 6 characters!"});
     }
+
+    if(repassword!=password) {
+        return res.json({msg: "Passwords dont match!"});
+    }
+
+
     const registerUser = new registerForm ({
         username:username,
         email:email,
