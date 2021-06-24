86f1b889b3252e9655b012de575ded96f3473b69
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-24 16:25:08 +0600

    Can access data from a register request

--- server/routes/userRoutes.routes.js
+++ server/routes/userRoutes.routes.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
 router.get("/login", (req,res)=>{
     res.send("Login page")
 });
 
 router.get("/register", (req,res)=>{
     res.sendFile("register.html", { root: "./views/users" });
+});
+
+router.post("/register", (req,res)=>{
+    const username = req.body.username;
+    const email = req.body.email;
+    res.send(
+      `<H1>user with Email - ${email} and Username - ${username} is requesting to login.</H1>`
+    );
 });
 
 module.exports=router;
