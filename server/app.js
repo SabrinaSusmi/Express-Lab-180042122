99625f95457150d141e1f0b5e9f59b4b491db274
SabrinaSusmi <sabrina.su512@gmail.com>
2021-07-02 11:30:51 +0600

    dashboard.ejs added and rendered

--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@ const userRoutes = require("./routes/userRoutes.routes");
 
 app.use(userRoutes);
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.status(200).sendFile("home.html", { root: "./views" });
+    //res.status(200).sendFile("home.html", { root: "./views" });
+    res.render("dashboard.ejs");
 });
 
 app.use((req, res) => {
