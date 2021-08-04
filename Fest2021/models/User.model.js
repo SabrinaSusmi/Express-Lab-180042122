a5aecbdab598d0f39c204fa4c19666fa8abf9cb0
SabrinaSusmi <sabrina.su512@gmail.com>
2021-08-04 21:28:20 +0600

    User model modified

--- Fest2021/models/User.model.js
+++ Fest2021/models/User.model.js
@@ -1,22 +1,26 @@
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+const mongoose = require('mongoose')
 
-const User = mongoose.model("User", UserSchema);
-module.exports = User;
+const registerForm = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    gender : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    }
+})
+
+module.exports = mongoose.model('users', registerForm);
