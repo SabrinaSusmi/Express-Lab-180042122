44eef01775dc4481ae976d63d621245bf3b8c359
SabrinaSusmi <sabrina.su512@gmail.com>
2021-08-05 18:15:13 +0600

    Team payment done

--- Fest2021/controllers/progContest.controller.js
+++ Fest2021/controllers/progContest.controller.js
@@ -225,4 +225,31 @@ const deletePC = (req, res) => {
       });
   };
 
-module.exports = { getPC, postPC, getPCList, deletePC, getInfoPC, editPC, selectPC };
+
+  const paymentDonePC = (req, res) => {
+    const id = req.params.id;
+  
+    progContest.findOne({ _id: id })
+      .then((team) => {
+        team.paid = team.total;
+        team
+          .save()
+          .then(() => {
+            let error = "Payment completed succesfully!";
+            req.flash("error", error);
+            res.redirect("/ProgContest/list");
+          })
+          .catch(() => {
+            let error = "Data could not be updated!";
+            req.flash("error", error);
+            res.redirect("/ProgContest/list");
+          });
+      })
+      .catch(() => {
+        let error = "Data could not be updated!";
+        req.flash("error", error);
+        res.redirect("/ProgContest/list");
+      });
+  };
+
+module.exports = { getPC, postPC, getPCList, deletePC, getInfoPC, editPC, selectPC, paymentDonePC };
--- Fest2021/routes/progContest.route.js
+++ Fest2021/routes/progContest.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { ensureAuthenticated, addUserData } = require("./../middlewares/auth.middleware");
 
-const { getPC, postPC, getPCList, deletePC, getInfoPC, editPC, selectPC } = require('../controllers/progContest.controller');
+const { getPC, postPC, getPCList, deletePC, getInfoPC, editPC, selectPC, paymentDonePC } = require('../controllers/progContest.controller');
 
 router.get("/register", ensureAuthenticated, addUserData, getPC);
 router.post("/register", ensureAuthenticated, addUserData, postPC);
@@ -12,5 +12,6 @@ router.get("/delete/:id", ensureAuthenticated, addUserData, deletePC);
 router.get('/editTeam/:id',ensureAuthenticated,addUserData,getInfoPC);
 router.post('/editTeam/:id',ensureAuthenticated,addUserData,editPC);
 router.get('/select/:id',ensureAuthenticated,addUserData,selectPC);
+router.get('/paymentDone/:id',ensureAuthenticated,addUserData,paymentDonePC);
 
 module.exports = router;
