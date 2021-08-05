fe02dab21dfadaa4682fb0ed113a8d2f2d912bf0
SabrinaSusmi <sabrina.su512@gmail.com>
2021-08-05 12:21:13 +0600

    prog contest - registration team view displayed

--- Fest2021/app.js
+++ Fest2021/app.js
@@ -45,8 +45,10 @@ app.use(express.urlencoded({ extended: false }));
 const indexRoutes = require("./routes/index.routes");
 const userRoutes = require("./routes/users.routes");
 const MORoutes = require("./routes/MathOlympiad.route");
+const PCRoutes = require("./routes/progContest.route");
 app.use(indexRoutes);
 app.use("/users", userRoutes);
 app.use("/MathOlympiad", MORoutes);
+app.use("/progContest", PCRoutes);
 
 module.exports = app;
